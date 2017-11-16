dc5da7a683727cf6f24825d06479b5f8058114c2
Johan PROTIN <jprotin@hipay.com>
2017-11-16 17:07:45 +0100

    init release

--- src/hipay_professional/hipay_professional.php
+++ src/hipay_professional/hipay_professional.php
@@ -42,7 +42,7 @@ class Hipay_Professional extends PaymentModule
     {
         $this->name = 'hipay_professional';
         $this->tab = 'payments_gateways';
-        $this->version = '1.0.6';
+        $this->version = '1.0.7';
         $this->module_key = 'ab188f639335535838c7ee492a2e89f8';
         $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
         $this->currencies = true;
