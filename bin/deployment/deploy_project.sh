b1b7ec9a1c74d65f858144a88b1bb95096e3f508
Johan PROTIN <jprotin@hipay.com>
2017-11-16 16:22:08 +0100

    bug artifactory

--- bin/deployment/deploy_project.sh
+++ bin/deployment/deploy_project.sh
@@ -13,10 +13,10 @@ sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port mkdir /dep
 echo "Transfert Artifact project for project $CIRCLE_PROJECT_REPONAME and branch $GITHUB_BRANCH"
 sshpass -p $PASS_DEPLOY scp -P $port ./package-ready-for-prestashop/*.zip root@docker-knock-auth.hipay.org:/deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$BRANCH
 
-echo "Deploy project in artifactory"
-sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port  "export DOCKER_API_VERSION=1.23 && docker exec " \
-    "jira-artifactory-pi.hipay-pos-platform.com" /tmp/jfrog rt u /deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$BRANCH/*.zip $CIRCLE_PROJECT_REPONAME/snapshot/ \
-    --flat=true --user=admin --password=$ARTIFACTORY_PASSWORD --url http://localhost:8081/artifactory/hipay/
+#echo "Deploy project in artifactory"
+#sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port  "export DOCKER_API_VERSION=1.23 && docker exec " \
+#    "jira-artifactory-pi.hipay-pos-platform.com" /tmp/jfrog rt u /deploy/project/artifactory/$CIRCLE_PROJECT_REPONAME/$BRANCH/*.zip $CIRCLE_PROJECT_REPONAME/snapshot/ \
+#    --flat=true --user=admin --password=$ARTIFACTORY_PASSWORD --url http://localhost:8081/artifactory/hipay/
 
 echo "Deploy project for project $CIRCLE_PROJECT_REPONAME and branch: $GITHUB_BRANCH - Port :$port"
 sshpass -p $PASS_DEPLOY ssh root@docker-knock-auth.hipay.org -p $port  "export DOCKER_API_VERSION=1.23 && docker exec " \
